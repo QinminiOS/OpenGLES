4a143f279a63197c32f6a5860fb9d9920641699b
qinmin <544652424@qq.com>
2017-04-11 06:51:22 +0800

    帧缓存

--- OpenGLES12-帧缓存/OpenGLES12-帧缓存/OpenGLESView.m
+++ OpenGLES12-帧缓存/OpenGLES12-帧缓存/OpenGLESView.m
@@ -17,10 +17,14 @@
     EAGLContext     *_context;
     GLuint          _colorRenderBuffer;
     GLuint          _frameBuffer;
+    GLuint          _frameBuffer1;
 
     GLuint          _program;
+    GLuint          _program1;
     GLuint          _vbo;
+    GLuint          _vbo1;
     GLuint          _texture;
+    GLuint          _texture1;
     int             _vertCount;
 }
 @end
@@ -46,8 +50,7 @@
         [self setupLayer];
         [self setupContext];
         [self setupGLProgram];
-        [self setupVBO];
-        [self setupTexure];
+        [self setupGLProgram1];
     }
     return self;
 }
@@ -59,6 +62,8 @@
     [self destoryRenderAndFrameBuffer];
     
     [self setupFrameAndRenderBuffer];
+    
+    [self setupFrameBuffer1];
     
     [self render];
 }
@@ -95,6 +100,7 @@
 
 - (void)setupFrameAndRenderBuffer
 {
+    // 窗口默认帧缓存
     glGenRenderbuffers(1, &_colorRenderBuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
     // 为 color renderbuffer 分配存储空间
@@ -108,13 +114,27 @@
                               GL_RENDERBUFFER, _colorRenderBuffer);
 }
 
+- (void)setupFrameBuffer1
+{
+    _texture1 = createTexture2D(GL_RGBA, self.frame.size.width, self.frame.size.height, NULL);
+    glGenFramebuffers(1, &_frameBuffer1);
+    // 设置为当前 framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer1);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture1, 0);
+}
+
 - (void)setupGLProgram
+{
+    NSString *vertFile1 = [[NSBundle mainBundle] pathForResource:@"vert.glsl" ofType:nil];
+    NSString *fragFile1 = [[NSBundle mainBundle] pathForResource:@"mosaic.glsl" ofType:nil];
+    _program1 = createGLProgramFromFile(vertFile1.UTF8String, fragFile1.UTF8String);
+}
+
+- (void)setupGLProgram1
 {
     NSString *vertFile = [[NSBundle mainBundle] pathForResource:@"vert.glsl" ofType:nil];
     NSString *fragFile = [[NSBundle mainBundle] pathForResource:@"frag.glsl" ofType:nil];
     _program = createGLProgramFromFile(vertFile.UTF8String, fragFile.UTF8String);
-    
-    glUseProgram(_program);
 }
 
 - (void)setupVBO
@@ -122,10 +142,10 @@
     _vertCount = 6;
     
     GLfloat vertices[] = {
-        0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // 右上
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // 左下
-        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f   // 左上
+        0.5f,  0.5f, 0.0f, 1.0f, 0.0f,   // 右上
+        0.5f, -0.5f, 0.0f, 1.0f, 1.0f,   // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  // 左下
+        -0.5f,  0.5f, 0.0f, 0.0f, 0.0f   // 左上
     };
     
     // 创建VBO
@@ -138,9 +158,30 @@
     glVertexAttribPointer(glGetAttribLocation(_program, "texcoord"), 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, NULL+sizeof(GL_FLOAT)*3);
 }
 
+- (void)setupVBO1
+{
+    _vertCount = 6;
+    
+    GLfloat vertices[] = {
+        1.0f,  1.0f, 0.0f, 1.0f, 1.0f,   // 右上
+        1.0f, -1.0f, 0.0f, 1.0f, 0.0f,   // 右下
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,  // 左下
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f   // 左上
+    };
+    
+    // 创建VBO
+    _vbo1 = createVBO(GL_ARRAY_BUFFER, GL_STATIC_DRAW, sizeof(vertices), vertices);
+    
+    glEnableVertexAttribArray(glGetAttribLocation(_program1, "position"));
+    glVertexAttribPointer(glGetAttribLocation(_program1, "position"), 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, NULL);
+    
+    glEnableVertexAttribArray(glGetAttribLocation(_program1, "texcoord"));
+    glVertexAttribPointer(glGetAttribLocation(_program1, "texcoord"), 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, NULL+sizeof(GL_FLOAT)*3);
+}
+
 - (void)setupTexure
 {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"wood" ofType:@"jpg"];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"1" ofType:@"jpg"];
     
     unsigned char *data;
     int size;
@@ -173,16 +214,44 @@
 #pragma mark - Render
 - (void)render
 {
+    /************************离屏渲染********************************************/
+    glUseProgram(_program1);
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer1);
+    
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glLineWidth(2.0);
     
     glViewport(0, 0, self.frame.size.width, self.frame.size.height);
     
+    [self setupVBO1];
+    [self setupTexure];
+    
     // 激活纹理
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, _texture);
-    glUniform1i(glGetUniformLocation(_program, "image"), 0);
+    glUniform1i(glGetUniformLocation(_program1, "image"), 0);
+    
+    // 索引数组
+    unsigned int indices1[] = {0,1,2,3,2,0};
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices1);
+    
+    /************************当前屏幕渲染********************************************/
+    glUseProgram(_program);
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
+    
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(2.0);
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    [self setupVBO];
+    
+    // 激活纹理
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, _texture1);
+    glUniform1i(glGetUniformLocation(_program, "image"), 1);
     
     // 索引数组
     unsigned int indices[] = {0,1,2,3,2,0};
